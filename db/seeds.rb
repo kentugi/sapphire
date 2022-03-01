ae9de6c13197cc4bd35895d187bccfb786c22d13
Andrey Kryuchkov <askryuchkov@ya.ru>
2022-03-01 14:52:29 +0800

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Booking.destroy_all
+Boat.destroy_all
+User.destroy_all
+
+puts 'Creating fake data...'
+15.times do
+  sailor = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.cell_phone_in_e164,
+    password: "123456789"
+  )
+  sailor.save!
+
+  boat = Boat.new(
+    title: Faker::JapaneseMedia::DragonBall.character,
+    description: Boat::BOAT_DESCRIPTIONS.sample,
+    location: Boat::LOCATIONS.sample,
+    boat_type: Boat::BOAT_TYPES.sample,
+    price: Boat::PRICES.sample,
+    user: sailor
+  )
+  boat.save!
+
+  client = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.cell_phone_in_e164,
+    password: "123456789"
+  )
+
+  client.save!
+
+  booking = Booking.new(
+    user: client,
+    boat: boat,
+    start_date: Date.today,
+    end_date: Date.today + rand(4..30)
+  )
+  booking.total = (booking.end_date - booking.start_date) * boat.price
+  booking.save!
+end
+puts 'Finished!'

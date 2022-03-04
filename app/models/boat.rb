9f26c99975cb390b668e7816d938e923f8efdb45
Andrey Kryuchkov <askryuchkov@ya.ru>
2022-03-04 15:26:12 +0800

    coordinates columns added to Boats

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,6 +1,8 @@
 class Boat < ApplicationRecord
   include PgSearch::Model
   multisearchable against: [:boat_type]
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 
   belongs_to :user
   has_many :bookings
